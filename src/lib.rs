f7727ff31e3e8ac49911c668daca2e0bf7519a01
Kamesh Sampath <kamesh.sampath@hotmail.com>
2023-02-19 08:13:52 +0530

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
+/// service module holds the REST services that are exposed as part of this library
+/// this module exposes a service named **HelloWorld**
+/// the **HelloWorld** service has following two REST URI
+/// - `/` - the root URI that always returns `Hello World!`. You can also use `DEFAULT_MESSAGE` environment variable to customize the `Hello World!` to a response of your choice
+/// - `/greet?<name=name>` - the greet URI returns a named greeting e.g `Hello! Jack!`. If no name Query Parameter is passed it returns `Hello! Anonymous!`
 pub mod service {
     use std::env;
 
     use axum::{http::StatusCode, routing::get, routing::IntoMakeService, Router};
 
+    /// Default message that will be returned with `/` REST path
+    pub const DEFAULT_MESSAGE: &str = "Hello World!";
+
+    /// greeter_routes holds all the services routes that are handled by path `/greet`
     pub mod greeter_routes;
 
+    /// HelloWorld service
     pub struct HelloWorld(Router);
 
     impl HelloWorld {
+        /// Returns an instance of `HelloWorld` with routes `/` and `/greet`
         pub fn new() -> Self {
             let app = Router::new()
                 .route("/", get(root))
@@ -15,27 +26,30 @@ pub mod service {
             HelloWorld(app)
         }
 
+        /// Return HelloWorld as axum router services
         pub fn service(self) -> IntoMakeService<Router> {
             self.0.into_make_service()
         }
 
+        /// Return a axum Router
         pub fn app(self) -> Router {
             self.0
         }
     }
 
+    /// The handler to handle the `/` request paths
     async fn root() -> (StatusCode, String) {
         if let Ok(message) = env::var("DEFAULT_MESSAGE") {
             return (StatusCode::OK, message);
         } else {
-            return (StatusCode::OK, "Hello World!".to_string());
+            return (StatusCode::OK, DEFAULT_MESSAGE.to_string());
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::service::HelloWorld as hello_world_service;
+    use crate::service::{HelloWorld as hello_world_service, DEFAULT_MESSAGE};
     use axum::{
         body::Body,
         http::{Request, StatusCode},
@@ -59,9 +73,9 @@ mod tests {
         let body_str = std::str::from_utf8(&body_bytes).unwrap();
 
         assert_eq!(
-            body_str, "Hello World!",
-            "Expecting `Hello World!` but got {}",
-            body_str
+            body_str, DEFAULT_MESSAGE,
+            "Expecting `{}` but got {}",
+            DEFAULT_MESSAGE, body_str
         );
     }
 
--- src/service/greeter_routes.rs
+++ src/service/greeter_routes.rs
@@ -3,11 +3,14 @@ use std::collections::HashMap;
 use axum::{extract::Query, http::StatusCode, Json};
 use serde::Serialize;
 
+// Greeting holds the message that will be returned as part of the greet request response
 #[derive(Serialize)]
 pub struct Greeting {
+    /// the greeting message e.g. Hello! World!
     pub message: String,
 }
-
+/// greet greets the user by Query Param `name`.
+/// If not `name` is provided it returns `Hello! Anonymous!"`
 pub async fn greet(Query(params): Query<HashMap<String, String>>) -> (StatusCode, Json<Greeting>) {
     let mut greeting = Greeting {
         message: "Hello! Anonymous!".to_string(),
@@ -18,4 +21,50 @@ pub async fn greet(Query(params): Query<HashMap<String, String>>) -> (StatusCode
     }
 
     (StatusCode::OK, Json(greeting))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::collections::HashMap;
+
+    use axum::extract::Query;
+
+    #[tokio::test]
+    async fn default_greeting() {
+        let expected_params: HashMap<String, String> = HashMap::new();
+        let query = Query(expected_params);
+        let (status_code, json_greeting) = super::greet(query).await;
+        assert_eq!(
+            status_code.as_u16(),
+            200,
+            "Expecting HTTP OK but got {}",
+            status_code.as_str()
+        );
+        let message = json_greeting.message.as_str();
+        assert_eq!(
+            message, "Hello! Anonymous!",
+            "Expected `Hello! Anonymous!` but got {}",
+            message
+        );
+    }
+    #[tokio::test]
+    async fn named_greeting() {
+        let mut expected_params: HashMap<String, String> = HashMap::new();
+        expected_params.insert("name".to_string(), "Jack".to_string());
+        let query = Query(expected_params);
+        let (status_code, json_greeting) = super::greet(query).await;
+        assert_eq!(
+            status_code.as_u16(),
+            200,
+            "Expecting HTTP OK but got {}",
+            status_code.as_str()
+        );
+        let message = json_greeting.message.as_str();
+        assert_eq!(
+            message, "Hello! Jack!",
+            "Expected `Hello! Jack!` but got {}",
+            message
+        );
+    }
 }
